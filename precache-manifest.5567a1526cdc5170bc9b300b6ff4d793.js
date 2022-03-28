32784f6dfda286e93b3842b569ea283e6b5c7147
valentin <jclyffe@DESKTOP-6EK9PFG.localdomain>
2022-03-28 18:22:10 -0300

    Updates

---
+++ precache-manifest.5567a1526cdc5170bc9b300b6ff4d793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4ff1500b4dcf74a095a",
+    "url": "/to-do_list/static/css/main.abc81477.chunk.css"
+  },
+  {
+    "revision": "a4ff1500b4dcf74a095a",
+    "url": "/to-do_list/static/js/main.a4ff1500.chunk.js"
+  },
+  {
+    "revision": "2e3e3a8fc8d7031001ae",
+    "url": "/to-do_list/static/js/1.2e3e3a8f.chunk.js"
+  },
+  {
+    "revision": "e455968d31fe081eb403",
+    "url": "/to-do_list/static/js/runtime~main.e455968d.js"
+  },
+  {
+    "revision": "7e17150f9cc0c26a98ca5c7751349f7f",
+    "url": "/to-do_list/static/media/noresults.7e17150f.png"
+  },
+  {
+    "revision": "9d653a663ac8b3964277de02e39f221b",
+    "url": "/to-do_list/index.html"
+  }
+];
